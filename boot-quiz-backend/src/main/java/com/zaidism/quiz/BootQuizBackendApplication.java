9017dcce889d6648afd683201b2f0ba5690634ec
zahid <zaidism7@gmail.com>
2026-02-28 03:07:41 +0530

    added project

---
+++ boot-quiz-backend/src/main/java/com/zaidism/quiz/BootQuizBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zaidism.quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootQuizBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootQuizBackendApplication.class, args);
+	}
+
+}
